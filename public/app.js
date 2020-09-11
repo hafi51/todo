535de8117d7c919e8e4ac56c345658764a90faaa
Muhammad Muaz <49091541+hafi51@users.noreply.github.com>
2020-09-11 11:42:47 +0500

    firebase added

--- public/app.js
+++ public/app.js
@@ -1,6 +1,7 @@
 list = document.getElementById('list')
 
-
+keyVal = 0
+myDatabase = firebase.database()
 function addTodo() {
     todoItem = document.getElementById('item')
         // del btn
@@ -20,34 +21,46 @@ function addTodo() {
         //li input
     if (todoItem.value != "") {
         li = document.createElement('li')
-        li.setAttribute('class', 'li')
         inputField = document.createElement('input')
-        inputField.setAttribute("type","text")
+        label = document.createElement('label')
+        li.setAttribute('class', 'li')
         inputField.value = todoItem.value
+        label.innerHTML = keyVal
+        inputField.setAttribute("type","text")
         inputField.setAttribute("id","inputField")
         inputField.setAttribute("class","input")
         inputField.setAttribute("disabled","")
+        li.appendChild(label)
         li.appendChild(inputField)
         li.appendChild(editBtn)
         li.appendChild(delBtn)
         list.appendChild(li)
         todoItem.value = ""
+        
+        for(i=0;i<1;i++){
+            myDatabase.ref(keyVal).set(`${inputField.value}`)
+            keyVal++
+        }       
     } else {
         alert("Enter Value.")
-    }
+    }   
 }
 
 function delAll() {
     list.innerHTML = ""
+    myDatabase.ref('/').remove()
+    keyVal = 0
 }
 
 function delItem(e) {
     e.parentNode.remove()
+    toRem = e.parentNode.firstChild.innerHTML
+    myDatabase.ref(toRem).remove()
 }
 
 function editItem(e) {
-    e.parentNode.firstChild.removeAttribute("disabled")
-    e.parentNode.firstChild.focus() 
+    e.parentNode.childNodes[1].removeAttribute("disabled")
+    e.parentNode.childNodes[1].focus() 
     e.innerHTML = "Update"
     e.setAttribute('onclick', 'updateItem(this);')
 }
@@ -55,4 +68,7 @@ function updateItem(e){
     e.innerHTML = "Edit Item"
     e.parentNode.firstChild.setAttribute("disabled","")
     e.setAttribute('onclick', 'editItem(this);')
+    toUpdate = e.parentNode.firstChild.innerHTML
+    updateVal = e.parentNode.childNodes[1].value
+    myDatabase.ref(toUpdate).set(updateVal)
 }
